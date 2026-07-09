bb44a57cf06d86752b898d458a5900e454a7542c
Selim López <sonicdash26@gmail.com>
2026-07-09 17:47:29 -0400

    create drizzle schema for menu entities

--- packages/web/src/lib/server/db/schema.ts
+++ packages/web/src/lib/server/db/schema.ts
@@ -1,9 +1,30 @@
+import { relations } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const task = sqliteTable('task', {
-	id: text('id')
-		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull(),
-	priority: integer('priority').notNull().default(1)
+export const categories = sqliteTable('categories', {
+	id: integer('id').primaryKey({ autoIncrement: true }),
+	name: text('name').notNull(),
+	displayOrder: integer('display_order').notNull()
 });
+
+export const products = sqliteTable('products', {
+	id: integer('id').primaryKey({ autoIncrement: true }),
+	categoryId: integer('category_id')
+		.notNull()
+		.references(() => categories.id, { onDelete: 'cascade' }),
+	name: text('name').notNull(),
+	description: text('description').notNull(),
+	price: integer('price').notNull(),
+	isAvailable: integer('is_available', { mode: 'boolean' }).notNull().default(true)
+});
+
+export const categoriesRelations = relations(categories, ({ many }) => ({
+	products: many(products)
+}));
+
+export const productsRelations = relations(products, ({ one }) => ({
+	category: one(categories, {
+		fields: [products.categoryId],
+		references: [categories.id]
+	})
+}));
